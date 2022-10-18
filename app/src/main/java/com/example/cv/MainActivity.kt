9465856a3e0725e07dd0dd6651db86a31f9a7f7e
Abdulhakim Edao <abdulhakim.edao1@gmail.com>
2022-10-18 00:05:26 -0500

    Option menu added.

--- app/src/main/java/com/example/cv/MainActivity.kt
+++ app/src/main/java/com/example/cv/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.cv
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.core.view.isVisible
 import com.example.cv.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
@@ -52,5 +54,41 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    // override option menu function to inflate the menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_items, menu)
+        return true
+    }
+    // override on options item selected function to handle the menu item click
+    override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
+        when(item.itemId){
+            R.id.linkedin -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/abdulhakim-edao-1b1b1b1b1/"))
+                startActivity(intent)
+            }
+            R.id.github -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Abdulhakim-Edao"))
+                startActivity(intent)
+            }
+            R.id.email -> {
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "text/html"
+                intent.putExtra(Intent.EXTRA_EMAIL, "abdulhakim.sirko@miu.edu")
+                intent.putExtra(Intent.EXTRA_SUBJECT, "CV Building App")
+                intent.putExtra(Intent.EXTRA_TEXT, "This is CV Building App test, \n\nThank you.")
+                startActivity(Intent.createChooser(intent, "Send Email"))
+            }
+            R.id.instagram -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/absolute_abb/"))
+                startActivity(intent)
+            }
+            R.id.whatsapp -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://wa.me/6124618437"))
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
