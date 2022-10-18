7052a2c2a0c27a84c06125e12cd6ab3f299901a3
Wendmagegn Kebede <wendmagegn.b@gmail.com>
2022-10-18 01:01:37 -0500

    About me

---
+++ app/src/main/java/com/example/cv/AboutMeFragment.kt
@@ -0,0 +1,9 @@
+package com.example.cv
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AboutMeFragment : Fragment(R.layout.fragment_aboutme)
--- app/src/main/java/com/example/cv/MyAdapter.kt
+++ app/src/main/java/com/example/cv/MyAdapter.kt
@@ -1,12 +1,8 @@
 package com.example.cv
 
-import android.content.Intent
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.adapter.FragmentViewHolder
-import com.example.cv.databinding.ActivityMainBinding
 
 class MyAdapter(fragmentActivity: FragmentActivity):FragmentStateAdapter(fragmentActivity) {
 
@@ -16,7 +12,7 @@ class MyAdapter(fragmentActivity: FragmentActivity):FragmentStateAdapter(fragmen
 
         return when(position){
             0->HomeFragment()
-            1->AboutmeFragment()
+            1->AboutMeFragment()
             2->ExperienceFragment()
             3->ContactFragment()
             else->Fragment()
